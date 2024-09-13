e9730123b76804184fd1bcf2ed07ec40feb719b4
Sabiq Mohammed <sabiq.mohammed4@gmail.com>
2024-09-13 13:49:25 +0530

    Add api_keys.txt to .gitignore

---
+++ lib/views/gallery/view/gallery.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Gallery extends StatelessWidget {
+  const Gallery({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column()),
+    );
+  }
+}
